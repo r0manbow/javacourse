ae0b49587ff499137e7f260a918211ac6cc654bf
r0manbow <139209306+r0manbow@users.noreply.github.com>
2023-07-14 09:43:18 +0200

    Update and rename Printer Errors to codewars_printer_errors.sh

--- codewars_printer_errors.sh
+++ codewars_printer_errors.sh
@@ -1,5 +1,5 @@
-//https://www.codewars.com/kata/56541980fa08ab47a0000040/train/shell
-//
+# https://www.codewars.com/kata/56541980fa08ab47a0000040/train/shell
+#
 #!/bin/bash
 printerError() {
 string=$1
