1ab5a11efb07eaec0e1ec913539511471396c57e
r0manbow <139209306+r0manbow@users.noreply.github.com>
2023-07-16 17:41:40 +0200

    Update codewars_jennys_secret_message.sh

--- codewars_jennys_secret_message.sh
+++ codewars_jennys_secret_message.sh
@@ -1,8 +1,8 @@
-# https://www.codewars.com/kata/55225023e1be1ec8bc000390/train/shell
-#
 #!/bin/bash
 if [ "$1" = "Johnny" ]; then
   echo "Hello. my Love!"
 else 
 	echo "Hello, $1!"
 fi
+
+# https://www.codewars.com/kata/55225023e1be1ec8bc000390/train/shell
