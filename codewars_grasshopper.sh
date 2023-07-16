a4f79287cd11ec46d506d137d74b35618d8b0b07
r0manbow <139209306+r0manbow@users.noreply.github.com>
2023-07-16 17:41:24 +0200

    Update codewars_grasshopper.sh

--- codewars_grasshopper.sh
+++ codewars_grasshopper.sh
@@ -1,6 +1,8 @@
-# https://www.codewars.com/kata/5625618b1fe21ab49f00001f/train/shell
-#
+#!/bin/bash
+
 say_hello() {
    echo "Hello, $1"
 }
 say_hello "$1"
+
+# https://www.codewars.com/kata/5625618b1fe21ab49f00001f/train/shell
